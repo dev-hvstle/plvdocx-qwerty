c072d7abf43381a44d6b8edeb154a0294e5c3467
rezarekt <theveyst12@gmail.com>
2021-06-15 12:26:15 +0800

    Update
    -Cancel Button Fixed

--- admin/register.php
+++ admin/register.php
@@ -10,7 +10,7 @@ include('includes/navbar.php');
  
 
                 <!-- Begin Page Content -->
-                <div class="modal fade" id="addadminprofile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="addadminprofile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
